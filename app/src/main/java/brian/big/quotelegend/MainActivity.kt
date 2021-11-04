23f3fde237ad7d9d009482dbe8aebb8d705ca885
Brian-big <briannbig@gmail.com>
2021-11-04 11:17:26 +0300

    homescreen ui

--- app/src/main/java/brian/big/quotelegend/MainActivity.kt
+++ app/src/main/java/brian/big/quotelegend/MainActivity.kt
@@ -3,12 +3,25 @@ package brian.big.quotelegend
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.VerifiedUser
+import androidx.compose.material.icons.rounded.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.widget.ConstraintLayout
 import brian.big.quotelegend.ui.theme.QuoteLegendTheme
+import brian.big.quotelegend.ui.theme.navy
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,8 +37,89 @@ class MainActivity : ComponentActivity() {
 }
 
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(showBackground = true)
 @Composable
 fun App(){
+    Scaffold(topBar = { AppTopBar()}, bottomBar = { AppBottomBar()}, floatingActionButton = {FloatingActionBtn()}) {
 
+    }
+}
+
+@Preview("AppTopBar")
+@Composable
+fun AppTopBar(){
+    TopAppBar(elevation = 10.dp) {
+    }
+}
+
+@Preview("AppBottomBar")
+@Composable
+fun AppBottomBar() {
+
+    var selectedState = remember {
+        "home"
+    }
+
+    BottomAppBar(
+        elevation = 10.dp
+    ) {
+        BottomNavigationItem(
+            icon= {
+                Icon(Icons.Filled.Home,"Home")
+            },
+            selectedContentColor= Color.White,
+            unselectedContentColor= Color.White.copy(alpha = 0.4f),
+            onClick = {
+                selectedState = "home"
+            },
+            selected = selectedState.equals("home")
+        )
+        BottomNavigationItem(
+            icon= {
+                Icon(Icons.Filled.Favorite,"Favorites")
+            },
+            selectedContentColor= Color.White,
+            unselectedContentColor= Color.White.copy(alpha = 0.4f),
+            onClick = {
+                selectedState = "favorites"
+            },
+            selected = selectedState.equals("favorites")
+        )
+
+        BottomNavigationItem(
+            icon= {
+                Icon(Icons.Filled.VerifiedUser,"Profile")
+            },
+            selectedContentColor= Color.White,
+            unselectedContentColor= Color.White.copy(alpha = 0.4f),
+            onClick = {
+                selectedState = "profile"
+            },
+            selected = selectedState.equals("profile")
+        )
+    }
+}
+
+@Preview()
+@Composable
+fun FloatingActionBtn(){
+    FloatingActionButton(onClick = { /*TODO*/ },
+        shape = RoundedCornerShape(50), contentColor = Color.White, backgroundColor = navy) {
+        Icon(Icons.Filled.Add, contentDescription = "add")
+    }
+}
+
+@Preview("Quote card preview")
+@Composable
+fun QuoteCard(modifier: Modifier = Modifier){
+    Row(
+        modifier
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+            .fillMaxWidth()){
+        Surface {
+            Image(imageVector = Icons.Rounded.FormatQuote,
+                contentDescription = "quote icon", modifier.size(50.dp))
+            Spacer(modifier.width(8.dp))
+        }
+    }
 }
--- app/src/main/java/brian/big/quotelegend/ui/theme/Color.kt
+++ app/src/main/java/brian/big/quotelegend/ui/theme/Color.kt
@@ -6,3 +6,6 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val navy = Color(0xFF203D46)
+val orange = Color(0xFFFF6F00)
